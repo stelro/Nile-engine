8b57be5d04ca8f910f30393264e9898ba38ae4fd
Ro Stelmach <stelmach.ro@gmail.com>
2019-08-26 08:42:01 +0300

    Add new texture2D class, is able to store and configure a textures in OpenGL

---
+++ src/renderer/texture2d.cc
@@ -0,0 +1,43 @@
+#include "renderer/texture2d.hh"
+
+#include <GL/glew.h>
+
+namespace nile {
+
+  Texture2D::Texture2D() noexcept
+      : m_width( 0 )
+      , m_height( 0 )
+      , m_internalFormat( GL_RGB )
+      , m_imageFormat( GL_RGB )
+      , m_wrapS( GL_REPEAT )
+      , m_wrapT( GL_REPEAT )
+      , m_filterMin( GL_LINEAR )
+      , m_filterMax( GL_LINEAR ) {
+    glGenTextures( 1, &this->m_id );
+  }
+
+  void Texture2D::generate( u32 width, u32 height, unsigned char *data ) noexcept {
+
+    this->m_width = width;
+    this->m_height = height;
+
+    // create texture
+    glBindTexture( GL_TEXTURE_2D, this->m_id );
+    glTexImage2D( GL_TEXTURE_2D, 0, this->m_internalFormat, width, height, 0, this->m_imageFormat,
+                  GL_UNSIGNED_BYTE, data );
+
+    // Set texture wrap and filter modes
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, this->m_wrapS );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, this->m_wrapT );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, this->m_filterMin );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, this->m_filterMax );
+
+    // unbind texture
+    glBindTexture( GL_TEXTURE_2D, 0 );
+  }
+
+  void Texture2D::bind() const noexcept {
+    glBindTexture(GL_TEXTURE_2D, this->m_id);
+  }
+
+}    // namespace nile
