60c74b92729e01aee34d7e77093a0f561997772c
Ro Stelmach <stelmach.ro@gmail.com>
2019-08-28 18:40:14 +0300

    Modified the InputManager class, and make it a Singleton class

--- include/core/input_manager.hh
+++ include/core/input_manager.hh
@@ -1,22 +1,45 @@
 #pragma once
 
 #include "core/nile.hh"
-
 #include <SDL2/SDL.h>
+
+#include <unordered_map>
 
 namespace nile {
 
   class InputManager {
   private:
+    InputManager() noexcept;
+    static InputManager *m_instance;
     SDL_Event m_event;
     bool m_shouldClose = false;
 
+    std::unordered_map<SDL_Keycode, bool> m_pressedKeys;
+
   public:
+    // Get singleton instance of the class
+    static InputManager *getInstance() noexcept;
+  
+    // Destory the singleton
+    static void destroy() noexcept;
+
     void update( [[maybe_unused]] f32 dt ) noexcept;
 
     [[nodiscard]] inline bool shouldClose() const noexcept {
       return m_shouldClose;
     }
+
+    // Checks if user has pressed any specifc key
+    [[nodiscard]] bool isKeyPressed( SDL_Keycode key ) const noexcept {
+      auto it = m_pressedKeys.find( key );
+      if ( it != m_pressedKeys.end() ) {
+        return it->second;
+      }
+      return false;
+    }
+
+    // Checks if specifc key has released
+    [[nodiscard]] bool isKeyHasReleased( SDL_Keycode key ) const noexcept;
   };
 
 }    // namespace nile
--- src/core/input_manager.cc
+++ src/core/input_manager.cc
@@ -2,14 +2,44 @@
 
 namespace nile {
 
+  InputManager *InputManager::m_instance = nullptr;
+
+  InputManager::InputManager() noexcept {
+    // Empty Constructor
+  }
+
+  InputManager *InputManager::getInstance() noexcept {
+    if ( !m_instance ) {
+      m_instance = new InputManager;
+    }
+    return m_instance;
+  }
+
+  void InputManager::destroy() noexcept {
+    if ( m_instance ) {
+      delete m_instance;
+      m_instance = nullptr;
+    }
+  }
+
   void InputManager::update( [[maybe_unused]] f32 dt ) noexcept {
 
-    while ( SDL_PollEvent(&m_event) ) {
-      if ( m_event.type == SDL_QUIT) {
+    while ( SDL_PollEvent( &m_event ) ) {
+      if ( m_event.type == SDL_QUIT ) {
         m_shouldClose = true;
       }
+
+      switch ( m_event.type ) {
+        case SDL_KEYDOWN:
+          m_pressedKeys[ m_event.key.keysym.sym ] = true;
+          break;
+        case SDL_KEYUP:
+          m_pressedKeys[ m_event.key.keysym.sym ] = false;
+          break;
+        default:
+          break;
+      }
     }
-      
   }
 
 }    // namespace nile
