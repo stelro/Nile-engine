eae8d2eb736f545e7d94350efbe1cccf3fc70a1f
Ro Stelmach <stelmach.ro@gmail.com>
2019-09-10 18:01:09 +0300

    Add debug mode to the settings class, which can be shared through the engine

--- src/core/settings.cc
+++ src/core/settings.cc
@@ -2,10 +2,14 @@
 
 namespace nile {
 
-  Settings::Settings( u32 width, u32 height, u32 flags, const std::string &title ) noexcept
+  Settings::Settings( u32 width, u32 height, u32 flags, bool debugMode,
+                      const std::string &title ) noexcept
       : m_width( width )
       , m_height( height )
       , m_windowFlags( flags )
+      , m_debugMode( debugMode )
       , m_windowTitle( title ) {}
+
+  Settings::~Settings() noexcept {}
 
 }    // namespace nile
