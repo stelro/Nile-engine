de18ff0d171a838a982b3f36e9130c210ef4bb11
Ro Stelmach <stelmach.ro@gmail.com>
2019-09-17 18:39:06 +0300

    Add game application logic, which every game should inherit

---
+++ include/Nile/application/game.hh
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Nile/core/nile.hh"
+
+namespace nile {
+
+  class Game {
+  public:
+    Game() noexcept = default;
+    virtual ~Game() noexcept = default;
+
+    NILE_DISABLE_COPY( Game )
+    NILE_DISABLE_MOVE( Game )
+
+    virtual void initialize() noexcept = 0;
+
+    virtual void draw( f32 deltaTime ) noexcept = 0;
+
+    virtual void update( f32 deltaTime ) noexcept = 0;
+  };
+
+}    // namespace nile
