b3b7febc72953eeb6bae22511f150b9222a4b266
Ro Stelmach <stelmach.ro@gmail.com>
2019-09-17 18:42:18 +0300

    Add new virtual methods that every derived renderer class should implement

--- include/Nile/renderer/base_renderer.hh
+++ include/Nile/renderer/base_renderer.hh
@@ -8,6 +8,8 @@ namespace nile {
     virtual void initRenderer() noexcept = 0;
     virtual void initWindow() noexcept = 0;
     virtual void init() noexcept = 0;
+    virtual void submitFrame() noexcept = 0;
+    virtual void endFrame() noexcept = 0;
     virtual void destroy() noexcept = 0;
   };
 
