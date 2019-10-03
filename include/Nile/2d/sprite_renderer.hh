3b017da691f5fbc93ac093e8a62395edb456d557
Stelmach Ro <stelmach.ro@gmail.com>
2019-10-03 18:58:57 +0300

    Replace Shader with ShaderSet object, also added notice message

--- include/Nile/2d/sprite_renderer.hh
+++ include/Nile/2d/sprite_renderer.hh
@@ -1,27 +1,34 @@
+/* ================================================================================
+$File: sprite_renderer.hh
+$Date: $
+$Revision: $
+$Creator: Rostislav Orestis Stelmach
+$Notice: $
+================================================================================ */
+
 #pragma once
 
 #include "Nile/core/nile.hh"
 #include "Nile/renderer/texture2d.hh"
 #include <glm/glm.hpp>
 
-#include <memory>
 
 namespace nile {
 
-  class Shader;
+  class ShaderSet;
 
   class SpriteRenderer {
   private:
-    std::shared_ptr<Shader> m_shader;
+    ShaderSet* m_shader;
     u32 m_quadVAO;
 
     void initRenderData() noexcept;
 
   public:
-    SpriteRenderer( std::shared_ptr<Shader> shader ) noexcept;
+    SpriteRenderer( ShaderSet* shader ) noexcept;
     ~SpriteRenderer() noexcept;
 
-    void draw( std::shared_ptr<Texture2D> texture, glm::vec2 position,
+    void draw( Texture2D* texture, glm::vec2 position,
                glm::vec2 size = glm::vec2( 10, 10 ), f32 roate = 0.0f,
                glm::vec3 color = glm::vec3( 1.0f ) ) noexcept;
   };
