9b29db0286111d4d66b6742654e46ef9d7c6d027
Stelmach Ro <stelmach.ro@gmail.com>
2019-10-03 19:08:08 +0300

    Add ShaderSet builder, which is responsible for shaderset creation
    An shaderSet consist usually by minimum 2 shaders ( vertex/fragment) but user can also define geometry shader.

---
+++ include/Nile/asset/builder/shaderset_builder.hh
@@ -0,0 +1,57 @@
+/* ================================================================================
+$File: shaderset_builder.hh
+$Date: $
+$Revision: $
+$Creator: Rostislav Orestis Stelmach
+$Notice: $
+================================================================================ */
+
+#pragma once
+
+#include "Nile/asset/builder/asset_builder.hh"
+#include "Nile/renderer/shaderset.hh"
+
+#include <string_view>
+
+namespace nile::AssetBuilder {
+
+  enum class ShaderType
+  {
+    VERTEX,
+    FRAGMENT,
+    GEOMETRY,
+    TESSALATION,
+    COMPUTE,
+    PROGRAM,
+    NONE
+  };
+
+  template <>
+  class Builder<ShaderSet> final {
+  private:
+    u32 loadShaderFromFile( std::string_view vshaderFile, std::string_view fshaderFile,
+                            std::string_view gshaderFile ) noexcept;
+
+    u32 compileShaders( const char *vSource, const char *fSource, const char *gSource ) noexcept;
+    void checkForCompileErrors( u32 object, ShaderType type ) noexcept;
+
+    std::string m_vertexPath {};
+    std::string m_fragmentPath {};
+    std::string m_geometryPath {};
+
+  public:
+    Builder() = default;
+    Builder( Builder && ) = default;
+    Builder( const Builder & ) = default;
+    Builder &operator=( Builder && ) = default;
+    Builder &operator=( const Builder & ) = default;
+
+
+    Builder &setVertexPath( const std::string &value ) noexcept;
+    Builder &setFragmentPath( const std::string &value ) noexcept;
+    Builder &setGeometryPath( const std::string &value ) noexcept;
+
+    [[nodiscard]] ShaderSet *build() noexcept;
+  };
+
+}    // namespace nile::AssetBuilder
---
+++ src/asset/builder/shaderset_builder.cc
@@ -0,0 +1,163 @@
+/* ================================================================================
+$File: shaderset_builder.cc
+$Date: $
+$Revision: $
+$Creator: Rostislav Orestis Stelmach
+$Notice: $
+================================================================================ */
+
+#include "Nile/asset/builder/shaderset_builder.hh"
+
+#include <GL/glew.h>
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+namespace nile::AssetBuilder {
+
+  Builder<ShaderSet> &Builder<ShaderSet>::setVertexPath( const std::string &value ) noexcept {
+    m_vertexPath = value;
+    return *this;
+  }
+
+  Builder<ShaderSet> &Builder<ShaderSet>::setFragmentPath( const std::string &value ) noexcept {
+    m_fragmentPath = value;
+    return *this;
+  }
+
+  Builder<ShaderSet> &Builder<ShaderSet>::setGeometryPath( const std::string &value ) noexcept {
+    m_geometryPath = value;
+    return *this;
+  }
+
+  [[nodiscard]] ShaderSet *Builder<ShaderSet>::build() noexcept {
+
+    ShaderSet *shader =
+        new ShaderSet( loadShaderFromFile( m_vertexPath, m_fragmentPath, m_geometryPath ) );
+
+    return shader;
+  }
+
+  u32 Builder<ShaderSet>::loadShaderFromFile( std::string_view vshaderFile,
+                                              std::string_view fshaderFile,
+                                              std::string_view gshaderFile ) noexcept {
+
+    u32 program_id = 0;
+
+    // 1. Retrieve the vertex/fragment source code from filePath
+    std::string vertexCode;
+    std::string fragmentCode;
+    std::string geometryCode;
+
+    // Open files
+    std::ifstream vertexShaderFile( vshaderFile.data() );
+    if ( !vertexShaderFile.is_open() ) {
+      log::error( "Failed to open { %s } file for vertex shader", vshaderFile.data() );
+    }
+
+    std::ifstream fragmentShaderFile( fshaderFile.data() );
+    if ( !fragmentShaderFile.is_open() ) {
+      log::error( "Failed to open { %s } file for fragment shader", fshaderFile.data() );
+    }
+
+    std::stringstream vShaderStream, fShaderStream;
+    // Read file's buffer contents into streams
+    vShaderStream << vertexShaderFile.rdbuf();
+    fShaderStream << fragmentShaderFile.rdbuf();
+
+    // close file handlers
+    vertexShaderFile.close();
+    fragmentShaderFile.close();
+
+    // Convert stream into string
+    vertexCode = vShaderStream.str();
+    fragmentCode = fShaderStream.str();
+
+    // If geometry shader path is present, also load a geometry shader
+    if ( gshaderFile.size() != 0 ) {
+      std::ifstream geometryShaderFile( gshaderFile.data() );
+      if ( !geometryShaderFile.is_open() ) {
+        log::error( "Failed to open { %s } file for geometry shader", gshaderFile.data() );
+      }
+
+      std::stringstream gShaderStream;
+      gShaderStream << geometryShaderFile.rdbuf();
+      geometryShaderFile.close();
+      geometryCode = gShaderStream.str();
+    }
+
+    const GLchar *vShaderCode = vertexCode.c_str();
+    const GLchar *fShaderCode = fragmentCode.c_str();
+    const GLchar *gShaderCode = geometryCode.c_str();
+
+    program_id =
+        compileShaders( vShaderCode, fShaderCode, gshaderFile.size() != 0 ? gShaderCode : nullptr );
+
+    return program_id;
+  }
+
+  u32 Builder<ShaderSet>::compileShaders( const char *vSource, const char *fSource,
+                                          const char *gSource ) noexcept {
+
+    GLuint sVertex, sFragment, gShader;
+    // Vertex Shader
+    sVertex = glCreateShader( GL_VERTEX_SHADER );
+    glShaderSource( sVertex, 1, &vSource, nullptr );
+    glCompileShader( sVertex );
+    checkForCompileErrors( sVertex, ShaderType::VERTEX );
+    // Fragment Shader
+    sFragment = glCreateShader( GL_FRAGMENT_SHADER );
+    glShaderSource( sFragment, 1, &fSource, nullptr );
+    glCompileShader( sFragment );
+    checkForCompileErrors( sFragment, ShaderType::FRAGMENT );
+
+    // If geometry shader source code is given, also compile geometry shader
+    if ( gSource != nullptr ) {
+      gShader = glCreateShader( GL_GEOMETRY_SHADER );
+      glShaderSource( gShader, 1, &gSource, nullptr );
+      glCompileShader( gShader );
+      checkForCompileErrors( gShader, ShaderType::GEOMETRY );
+    }
+
+    // Shader Program
+    u32 program_id = glCreateProgram();
+    glAttachShader( program_id, sVertex );
+    glAttachShader( program_id, sFragment );
+
+    if ( gSource != nullptr )
+      glAttachShader( program_id, gShader );
+    glLinkProgram( program_id );
+
+    checkForCompileErrors( program_id, ShaderType::PROGRAM );
+    // Delete the shaders as they're linked into our program now and no longer necessery
+    glDeleteShader( sVertex );
+    glDeleteShader( sFragment );
+
+    if ( gSource != nullptr )
+      glDeleteShader( gShader );
+
+    return program_id;
+  }
+
+  void Builder<ShaderSet>::checkForCompileErrors( u32 object, ShaderType type ) noexcept {
+
+    GLint success;
+    char infoLog[ 1024 ];
+    if ( type != ShaderType::PROGRAM ) {
+      glGetShaderiv( object, GL_COMPILE_STATUS, &success );
+      if ( !success ) {
+        glGetShaderInfoLog( object, 1024, NULL, infoLog );
+        log::error( "::SHADER: Compile-time error: Type %s\n%s\n", type, infoLog );
+      }
+    } else {
+      glGetProgramiv( object, GL_LINK_STATUS, &success );
+      if ( !success ) {
+        glGetProgramInfoLog( object, 1024, NULL, infoLog );
+        log::error( "::SHADER: Link-time error: Type: %s\n%s\n", type, infoLog );
+      }
+    }
+  }
+
+
+}    // namespace nile::AssetBuilder
