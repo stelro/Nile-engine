4dfcac9a0dccbd038d40b2352a7815353cc6777b
Stelmach Ro <stelmach.ro@gmail.com>
2019-10-03 19:04:22 +0300

    Add asset class which represent an Asset in the Game Engine
    This class is representing an abstract Asset File, and every derived class that inherit from this class, can be stored in asset manager container!

---
+++ include/Nile/asset/asset.hh
@@ -0,0 +1,53 @@
+/* ================================================================================
+$File: asset.hh
+$Date: $
+$Revision: $
+$Creator: Rostislav Orestis Stelmach
+$Notice: $
+================================================================================ */
+
+#pragma once
+
+#include "Nile/core/reference.hh"
+
+namespace nile {
+
+  class AssetContainer;
+
+  class Asset {
+    friend class AssetContainer;
+
+  private:
+    Reference *m_reference;
+    std::string m_fileName;
+    std::string m_assetName;
+
+  public:
+    Asset() noexcept
+        : m_reference( new Reference() ) {}
+    ~Asset() noexcept {
+      delete m_reference;
+    }
+
+    void setFileName( const std::string &name ) noexcept {
+      m_fileName = name;
+    }
+
+    void setAssetName( const std::string &name ) noexcept {
+      m_assetName = name;
+    }
+
+    std::string getFileName() const noexcept {
+      return m_fileName;
+    }
+
+    std::string getAssetName() const noexcept {
+      return m_assetName;
+    }
+
+    u32 getRefCount() const noexcept {
+      return m_reference->getRefCount();
+    }
+  };
+
+}    // namespace nile
