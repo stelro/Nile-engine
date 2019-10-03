a8fb1747cf64d20c372e9e2446c1c3ae211bf7da
Stelmach Ro <stelmach.ro@gmail.com>
2019-10-03 19:13:08 +0300

    Add notice message, also added method which returns assetmanager

--- include/Nile/platform/x11/game_host_x11.hh
+++ include/Nile/platform/x11/game_host_x11.hh
@@ -1,4 +1,13 @@
+/* ================================================================================
+$File: game_host_x11.hh
+$Date: $
+$Revision: $
+$Creator: Rostislav Orestis Stelmach
+$Notice: $
+================================================================================ */
+
 #pragma once
+
 
 #include "Nile/platform/game_host.hh"
 
@@ -25,6 +34,7 @@ namespace nile {
       [[nodiscard]] std::shared_ptr<Settings> getSettings() const noexcept override;
       [[nodiscard]] std::shared_ptr<BaseRenderer> getRenderer() const noexcept override;
       [[nodiscard]] std::shared_ptr<InputManager> getInputManager() const noexcept override;
+      [[nodiscard]] std::shared_ptr<AssetManager> getAssetManager() const noexcept override;
     };
 
   }    // namespace X11
