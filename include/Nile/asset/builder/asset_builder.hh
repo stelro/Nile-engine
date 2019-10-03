27c6794c189d3686ba6c38a670ffe997c54a02fa
Stelmach Ro <stelmach.ro@gmail.com>
2019-10-03 19:06:57 +0300

    Add AssetBuilder, is base class for Builders that can be created by assetmanager class

---
+++ include/Nile/asset/builder/asset_builder.hh
@@ -0,0 +1,20 @@
+/* ================================================================================
+$File: asset_builder.hh
+$Date: $
+$Revision: $
+$Creator: Rostislav Orestis Stelmach
+$Notice: $
+================================================================================ */
+
+#pragma once
+
+namespace nile {
+
+  namespace AssetBuilder {
+
+    template <typename T>
+    class Builder;
+
+  }
+
+}    // namespace nile
