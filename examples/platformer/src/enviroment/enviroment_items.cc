da527c16826bcd4fe7fcf9547bb9390f97f52f29
Stelmach Ro <stelmach.ro@gmail.com>
2019-10-09 02:09:33 +0300

    Add seperete class that represent enviroment items

--- examples/platformer/src/enviroment/enviroment_items.cc
+++ examples/platformer/src/enviroment/enviroment_items.cc
@@ -1,4 +1,6 @@
 #include "enviroment_items.hh"
+#include <Nile/2d/sprite.hh>
+#include <Nile/asset/builder/shaderset_builder.hh>
 #include <Nile/renderer/shaderset.hh>
 #include <Nile/renderer/texture2d.hh>
 
@@ -7,32 +9,54 @@ namespace platformer {
   using namespace nile;
 
   EnviromentItems::EnviromentItems( const std::shared_ptr<nile::Settings> &settings,
-                                    const std::shared_ptr<nile::AssetManager> &manager ) noexcept
+                                    const std::shared_ptr<nile::AssetManager> &manager,
+                                    const std::shared_ptr<nile::SpriteRenderer> &renderer,
+                                    const std::shared_ptr<Camera2D> &camera ) noexcept
       : m_settings( settings )
-      , m_assetManager( manager ) {}
+      , m_assetManager( manager )
+      , m_spriteRenderer( renderer )
+      , m_camera( camera ) {}
 
   void EnviromentItems::initialize() noexcept {
 
+    auto spriteSheetShader = m_assetManager->createBuilder<ShaderSet>()
+                                 .setVertexPath( "../assets/shaders/spritesheet_vertex.glsl" )
+                                 .setFragmentPath( "../assets/shaders/spritesheet_fragment.glsl" )
+                                 .build();
+
+
+    m_assetManager->storeAsset<ShaderSet>( "envitems_shader", spriteSheetShader );
+
+
     auto torchTexture = m_assetManager->loadAsset<Texture2D>(
         "torch_flame", "../assets/textures/torch/flames.png" );
-    
-    auto torchHandleTex = m_assetManager->loadAsset<Texture2D>(
-        "torch_handle", "../assets/textures/torch/torch_handle.png");
-    
 
-    auto spriteSheetShader = m_assetManager->getAsset<ShaderSet>( "spritesheet_shader" );
+    m_staticSprites.emplace_back( Sprite( m_assetManager->loadAsset<Texture2D>(
+        "torch_handle", "../assets/textures/torch/torch_handle.png" ) ) );
+    m_staticSprites.back().setPosition( glm::vec2( 200, 200 ) );
+    m_staticSprites.back().setSize( glm::vec2( 13.2f ) );
+    m_staticSprites.back().setColor( glm::vec3( 1.0f ) );
+
+
 
     m_torchSpriteSheet =
         std::make_unique<SpriteSheet>( spriteSheetShader, torchTexture, glm::vec2( 16, 16 ) );
-    m_torchSpriteSheet->scale(2.8f);
-  
+    m_torchSpriteSheet->scale( 1.0f );
   }
 
   void EnviromentItems::update( f32 deltaTime ) noexcept {}
 
   void EnviromentItems::draw( f32 deltaTime ) noexcept {
 
-    m_torchSpriteSheet->playAnimation( glm::vec2( 40, 40 ), 80 );
+    for ( const auto &i : m_staticSprites ) {
+      m_spriteRenderer->draw( i );
+    }
+
+    m_torchSpriteSheet->playAnimation( glm::vec2( 200, 190 ), 80 );
+
+    m_assetManager->getAsset<ShaderSet>( "envitems_shader" )
+        ->use()
+        .SetMatrix4( "projection", m_camera->getCameraMatrix() );
   }
 
 
--- examples/platformer/src/enviroment/enviroment_items.hh
+++ examples/platformer/src/enviroment/enviroment_items.hh
@@ -1,12 +1,15 @@
 #pragma once
 
+#include <Nile/2d/2d_camera.hh>
+#include <Nile/2d/sprite.hh>
+#include <Nile/2d/sprite_renderer.hh>
 #include <Nile/2d/sprite_sheet.hh>
+#include <Nile/2d/spritesheet_context.hh>
 #include <Nile/asset/asset_manager.hh>
 #include <Nile/core/nile.hh>
 #include <Nile/core/settings.hh>
-#include <Nile/2d/spritesheet_context.hh>
-
 #include <memory>
+#include <vector>
 
 namespace platformer {
 
@@ -15,13 +18,19 @@ namespace platformer {
   class EnviromentItems {
   private:
     std::unique_ptr<nile::SpriteSheet> m_torchSpriteSheet;
-    std::unique_ptr<nile::SpriteSheetContext> m_torchContext;
 
     std::shared_ptr<nile::Settings> m_settings;
     std::shared_ptr<nile::AssetManager> m_assetManager;
+    std::shared_ptr<nile::SpriteRenderer> m_spriteRenderer;
+    std::shared_ptr<nile::Camera2D> m_camera;
+    std::vector<nile::Sprite> m_staticSprites;
+
   public:
     EnviromentItems( const std::shared_ptr<nile::Settings> &settings,
-                     const std::shared_ptr<nile::AssetManager> &manager ) noexcept;
+                     const std::shared_ptr<nile::AssetManager> &manager,
+                     const std::shared_ptr<nile::SpriteRenderer> &renderer,
+                     const std::shared_ptr<nile::Camera2D> &camera ) noexcept;
+
     void initialize() noexcept;
     void update( f32 deltaTime ) noexcept;
     void draw( f32 deltaTime ) noexcept;
