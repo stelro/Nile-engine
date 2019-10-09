4d2f17ecca37349f1b1b1c0e165baef15620b4ce
Stelmach Ro <stelmach.ro@gmail.com>
2019-10-09 10:12:01 +0300

    Add renderable component, which represents and renderable object in the world space.

---
+++ include/Nile/ecs/components/renderable.hh
@@ -0,0 +1,19 @@
+/* ================================================================================
+$File: renderable.hh
+$Date: 09 Oct 2019 $
+$Revision: $
+$Creator: Rostislav Orestis Stelmach
+$Notice: $
+================================================================================ */
+
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace nile {
+
+  struct Renderable {
+    glm::vec3 color;
+  };
+
+}    // namespace nile
