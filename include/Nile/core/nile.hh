f6af4cb2bcaf681a15c24a0d7e29afee58c81f8d
Stelmach Ro <stelmach.ro@gmail.com>
2019-10-09 10:07:26 +0300

    Deleted nile.hh file, it is now seperated in 3 other header files

--- include/Nile/core/nile.hh
+++ include/Nile/core/nile.hh
@@ -11,23 +11,6 @@ $Notice: $
 #include "Nile/core/logger.hh"
 #include <cstdint>
 
-namespace nile {
-
-  // Types aliases
-  using i8 = int8_t;
-  using i16 = int16_t;
-  using i32 = int32_t;
-  using i64 = int64_t;
-  using u8 = uint8_t;
-  using u16 = uint16_t;
-  using u32 = uint32_t;
-  using u64 = uint32_t;
-  using f32 = float;
-  using f64 = double;
-  using usize = std::size_t;
-  using uptr = uintptr_t;
-
-}    // namespace nile
 
 // Helpers macros used to disable copy/move
 // constructors
@@ -40,20 +23,4 @@ namespace nile {
   Class( const Class && ) = delete;                                                                \
   Class &operator=( const Class && ) = delete;
 
-#define ASSERT_M( condition, msg )                                                                 \
-   do {                                                                                           \
-     if ( !( condition ) ) {                                                                      \
-       nile::log::fatal( "%s(%d): Assertion failed with error - %s\n", __FILE__, __LINE__, msg ); \
-                                                                                                  \
-    }                                                                                              \
-  }                                                                                                \
-  while ( 0 )                                                                                      \
-    ;
-
-#define ASSERT( condition )                                                                        \
-  do {                                                                                             \
-    if ( !( condition ) ) {                                                                        \
-      nile::log::fatal( "%s(%d): Assertion failed\n", __FILE__, __LINE__ );                        \
-    }                                                                                              \
-  } while ( 0 );
 
