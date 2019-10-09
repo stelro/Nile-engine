ff743eeb910cbc10647334ac222788aa8850889f
Stelmach Ro <stelmach.ro@gmail.com>
2019-10-09 10:13:20 +0300

    Add ECS System class, which is base class for every derived ecs system in the engine.

---
+++ include/Nile/ecs/ecs_system.hh
@@ -0,0 +1,21 @@
+/* ================================================================================
+$File: ecs_system.hh
+$Date: 09 Oct 2019 $
+$Revision: $
+$Creator: Rostislav Orestis Stelmach
+$Notice: $
+================================================================================ */
+
+#pragma once
+
+#include "Nile/core/types.hh"
+#include <set>
+
+namespace nile {
+
+  class System {
+  public:
+    std::set<Entity> m_entities;
+  };
+
+}    // namespace nile
