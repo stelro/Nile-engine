0e3b28997911b8432214be79a44ef7d71bc4f327
Stelmach Ro <stelmach.ro@gmail.com>
2019-10-20 10:39:54 +0300

    Add view model, since now the engine will be able to render 3D too

--- examples/platformer/assets/shaders/sprite_vertex.glsl
+++ examples/platformer/assets/shaders/sprite_vertex.glsl
@@ -4,11 +4,12 @@
 layout( location = 0 ) in vec4 vertex;
 
 uniform mat4 model;
+uniform mat4 view;
 uniform mat4 projection;
 
 out vec2 TexCoords;
 
 void main() {
   TexCoords = vertex.zw;
-  gl_Position = projection * model * vec4( vertex.xy, 0.0, 1.0 );
+  gl_Position = projection * view * model * vec4( vertex.xy, 0.0, 1.0 );
 }
