b5d890969ae3919cf9f7031266090e3510d8c9f8
Stelmach Ro <stelmach.ro@gmail.com>
2019-10-20 10:56:42 +0300

    Working on 3D rendering system

--- include/Nile/renderer/rendering_system.hh
+++ include/Nile/renderer/rendering_system.hh
@@ -9,18 +9,27 @@ $Notice: $
 #pragma once 
 
 #include "Nile/ecs/ecs_system.hh"
+#include "Nile/core/types.hh"
 
 #include <memory>
 
 namespace nile {
   
   class Coordinator;
+  class ShaderSet;
 
   class RenderingSystem : public System {
     private:
       std::shared_ptr<Coordinator> m_ecsCoordinator;
+      u32 m_vao;
+      ShaderSet* m_shader;
+      void initRenderData() noexcept;
+
+
     public:
-      void init(const std::shared_ptr<Coordinator>& coordinator) noexcept;
+      RenderingSystem(const std::shared_ptr<Coordinator>& coordinator, ShaderSet* shader) noexcept;
+      void create() noexcept;
       void update(float dt) noexcept;
+      void render(float dt) noexcept;
   };
 }
--- src/renderer/rendering_system.cc
+++ src/renderer/rendering_system.cc
@@ -2,26 +2,119 @@
 #include "Nile/ecs/components/renderable.hh"
 #include "Nile/ecs/components/transform.hh"
 #include "Nile/ecs/ecs_coordinator.hh"
+#include "Nile/renderer/shaderset.hh"
+#include "Nile/ecs/components/mesh_component.hh"
+#include "Nile/ecs/components/renderable.hh"
+#include "Nile/ecs/components/transform.hh"
+#include "Nile/renderer/texture2d.hh"
 
 #include <glm/glm.hpp>
+#include <GL/glew.h>
+#include <glm/gtc/matrix_transform.hpp>
 
 namespace nile {
 
-  void RenderingSystem::init( const std::shared_ptr<Coordinator> &coordinator ) noexcept {
-    m_ecsCoordinator = coordinator;
+  RenderingSystem::RenderingSystem( const std::shared_ptr<Coordinator> &coordinator, ShaderSet* shader ) noexcept
+      : m_ecsCoordinator( coordinator ) 
+      , m_shader(shader)
+      {}
+
+  void RenderingSystem::create() noexcept {
+    this->initRenderData();
   }
 
-  void RenderingSystem::update( float dt ) noexcept {
-   
-    for ( const auto &entity : m_entities ) {
-      auto &tranform = m_ecsCoordinator->getComponent<Transform>( entity );
-      auto &render = m_ecsCoordinator->getComponent<Renderable>( entity );
+  void RenderingSystem::update( float dt ) noexcept {}
 
-      tranform.position *= dt;
-      render.color *= dt;
-       log::print( "Hello from system render\n" );
+  void RenderingSystem::render(float dt) noexcept {
+    
+    for ( const auto& entity : m_entities ) {
+      
+
+      auto& mesh = m_ecsCoordinator->getComponent<MeshComponent>(entity);
+      auto& transform = m_ecsCoordinator->getComponent<Transform>(entity);
+      auto& renderable = m_ecsCoordinator->getComponent<Renderable>(entity);
+
+      glm::mat4 model = glm::mat4(1.0f);
+      model = glm::translate(model, transform.position);
+      model = glm::rotate(model , glm::radians(0.0f), glm::vec3(1.0f, 0.3f, 0.5f));
+
+      this->m_shader->SetMatrix4("model", model);
+      this->m_shader->SetVector3f("spriteColor", renderable.color);
+
+      glActiveTexture(GL_TEXTURE0);
+      mesh.texture->bind();
+ 
+      glBindVertexArray(this->m_vao);
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+      glBindVertexArray(0);
 
     }
+
+  }
+
+  void RenderingSystem::initRenderData() noexcept {
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+
+    u32 vbo;
+    glGenVertexArrays(1, &this->m_vao);
+    glGenBuffers(1, &vbo);
+
+    glBindVertexArray(m_vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float))); 
+    glEnableVertexAttribArray(1);
+
+    m_shader->SetInteger("image", 0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+    glBindVertexArray(0);
   }
 
 }    // namespace nile
