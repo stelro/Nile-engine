2d8b889ba58542e295872ab897d45f8a29f8a233
Stelmach Ro <stelmach.ro@gmail.com>
2019-11-01 09:00:33 +0200

    Fix enabled gl_blend

--- src/renderer/opengl_renderer.cc
+++ src/renderer/opengl_renderer.cc
@@ -68,7 +68,7 @@ namespace nile {
 
 //    glEnable( GL_CULL_FACE );
     glEnable( GL_DEPTH_TEST );
-    glEnable( GL_BLEND );
+//    glEnable( GL_BLEND );
     glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
     m_isRunning = true;
   }
