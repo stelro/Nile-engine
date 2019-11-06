5cda89c433628fec835a4c5f46e2f2849027c095
Stelmach Ro <stelmach.ro@gmail.com>
2019-11-06 14:02:34 +0200

    Add support to LogType enum class for bitwise manipulation

--- include/Nile/log/log_type.hh
+++ include/Nile/log/log_type.hh
@@ -1,15 +1,29 @@
 #pragma once
 
+#include <type_traits>
+
 namespace nile {
 
-  enum class LogType { 
-    NONE = 0x00, 
-    NOTICE = 0x01, 
-    ERROR = 0x02, 
-    DISPLAY = 0x04, // printed out only in editor console
-    WARNING = 0x08,
-    PRINT = 0x10, 
-    FATAL = 0x20  // printed out only in file and stderr
+  enum class LogType {
+    NONE      = 0x00,
+    NOTICE    = 0x01,
+    ERROR     = 0x02,
+    DISPLAY   = 0x04,    // printed out only in editor console
+    WARNING   = 0x08,
+    PRINT     = 0x10,
+    FATAL     = 0x20    // printed out only in file and stderr
   };
 
-}
+  inline LogType operator|( LogType lhs, LogType rhs ) noexcept {
+
+    return static_cast<LogType>( static_cast<std::underlying_type_t<LogType>>( lhs ) |
+                                 static_cast<std::underlying_type_t<LogType>>( rhs ) );
+  }
+
+  inline LogType operator&( LogType lhs, LogType rhs ) noexcept {
+
+    return static_cast<LogType>( static_cast<std::underlying_type_t<LogType>>( lhs ) &
+                                 static_cast<std::underlying_type_t<LogType>>( rhs ) );
+  }
+
+}    // namespace nile
