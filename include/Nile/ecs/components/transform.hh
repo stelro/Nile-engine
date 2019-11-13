af47c30b734fe7661f3812206cbe9a16801aeb7f
Stelmach Ro <stelmach.ro@gmail.com>
2019-11-13 08:59:46 +0200

    Add some new constructors the help the user with the transform struct

--- include/Nile/ecs/components/transform.hh
+++ include/Nile/ecs/components/transform.hh
@@ -19,11 +19,29 @@ namespace nile {
     Transform( const glm::vec3 &p, const glm::vec3 &s, f32 r ) noexcept
         : position( p )
         , scale( s )
-        , rotation( r ) {}
+        , xRotation( r )
+        , yRotation( r )
+        , zRotation( r ) {}
+
+    Transform( const glm::vec3 &p, const glm::vec3 &s ) noexcept
+        : position( p )
+        , scale( s )
+        , xRotation( 0.0f )
+        , yRotation( 0.0f )
+        , zRotation( 0.0f ) {}
+
+    Transform( const glm::vec3 &p, const glm::vec3 &s, f32 x, f32 y, f32 z ) noexcept
+        : position( p )
+        , scale( s )
+        , xRotation( x )
+        , yRotation( y )
+        , zRotation( z ) {}
 
     glm::vec3 position {1.0f};
     glm::vec3 scale {1.0f};
-    f32 rotation {};
+    f32 xRotation {};
+    f32 yRotation {};
+    f32 zRotation {};
   };
 
 }    // namespace nile
