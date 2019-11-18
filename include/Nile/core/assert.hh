e46b21bc61449c7a5503aa743653b56ed3a0326f
Stelmach Ro <stelmach.ro@gmail.com>
2019-11-18 10:42:53 +0200

    Fix assert macros, with better errors and call to std::abort() if the assertion failed.

--- include/Nile/core/assert.hh
+++ include/Nile/core/assert.hh
@@ -9,18 +9,27 @@ $Notice: $
 #pragma once
 
 #include <cstdio>
+#include <cstdlib>
+
+#define STR( x ) #x
 
 #define ASSERT_M( condition, msg )                                                                 \
   do {                                                                                             \
     if ( !( condition ) ) {                                                                        \
-      printf( "%s(%d): Assertion failed with error - %s\n", __FILE__, __LINE__, msg );   \
+      printf( "\n\033[0;31mAssertion Failed ->\033[0m [ %s ]\n - function: %s\n - file: %s\n - "   \
+              "line: %d\n - reason: %s\n\n",                                                                      \
+              STR( condition ), __PRETTY_FUNCTION__, __FILE__, __LINE__, msg );                         \
+      std::abort();                                                                                \
     }                                                                                              \
   } while ( 0 );
 
 #define ASSERT( condition )                                                                        \
   do {                                                                                             \
     if ( !( condition ) ) {                                                                        \
-      printf( "%s(%d): Assertion failed\n", __FILE__, __LINE__ );                        \
+      printf( "\n\033[0;31mAssertion Failed ->\033[0m [ %s ]\n - function: %s\n - file: %s\n - "   \
+              "line: %d\n\n",                                                                      \
+              STR( condition ), __PRETTY_FUNCTION__, __FILE__, __LINE__ );                         \
+      std::abort();                                                                                \
     }                                                                                              \
   } while ( 0 );
 
